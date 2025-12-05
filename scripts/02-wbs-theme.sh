ff19317fdba1b6f6f615b4fb41889d375e2e533a
MattIPv4 <me@mattcowley.co.uk>
2025-12-05 22:10:34 +0000

    Add script to apply WBS Tailwind theme

---
+++ scripts/02-wbs-theme.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Read in the patch directory from the first argument.
+PATCH_DIR="$1"
+if [ -z "$PATCH_DIR" ]; then
+    echo "Error: Patch directory argument is required."
+    exit 1
+fi
+
+# Add the WBS theme colors to the Tailwind CSS configuration.
+git apply "$PATCH_DIR/patches/02-wbs-theme.patch"
+
+# Replace all occurrences of the Alveus theme colors with the WBS theme colors.
+sed -i '' 's/alveus-green/wbs-blue/g' $(find src -name '*.tsx' -o -name '*.ts' -o -name '*.css')
+sed -i '' 's/alveus-tan/wbs-grey/g' $(find src -name '*.tsx' -o -name '*.ts' -o -name '*.css')
