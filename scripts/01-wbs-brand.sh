85140d004fc403b00f02dbca6482945659226b86
MattIPv4 <me@mattcowley.co.uk>
2025-12-05 22:13:09 +0000

    Add script to apply WBS brand

---
+++ scripts/01-wbs-brand.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Read in the patch directory from the first argument.
+PATCH_DIR="$1"
+if [ -z "$PATCH_DIR" ]; then
+    echo "Error: Patch directory argument is required."
+    exit 1
+fi
+
+# Update the extension branding to World Bird Sanctuary (WBS).
+# TODO: Update other references (e.g., welcome card)
+git apply "$PATCH_DIR/patches/01-wbs-brand.patch"
+
+# Remove the Alveus logo
+rm src/assets/alveus.png
+
+# Copy in the WBS logo
+cp "$PATCH_DIR/assets/wbs.png" src/assets/wbs.png
